f3134e70009b3886c333ccfa2a25486935e10a77
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-04 23:06:41 +0300

    feat: complete intermediate mini drills challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-03-data-types/04-mini-drills.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-03-data-types/04-mini-drills.js
@@ -20,3 +20,13 @@ p3 = 0;
 let n = null;
 let u;
 
+// ===============================
+// 🟡 Intermediate (7–12)
+// ===============================
+
+console.log(Number("50") * 2);
+console.log(String(false));
+console.log(Boolean(null));
+console.log(Boolean("true"));
+console.log(Boolean(0));
+console.log(typeof ("5" * 2));
