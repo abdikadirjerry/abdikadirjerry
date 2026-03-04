0087faa8f3d70588680c64de2e7cdfe5f399a9c2
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-04 22:58:52 +0300

    feat: complete easy null vs undefined challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-03-data-types/02-null-vs-undefined.js
@@ -0,0 +1,35 @@
+// ============================================
+// 02-null-vs-undefined.js
+// ============================================
+
+// ===============================
+// 🟢 Easy (1–8)
+// ===============================
+
+// 1
+let a;
+console.log(a);
+
+// 2
+let b = null;
+console.log(b);
+
+// 3
+console.log(typeof null);
+
+// 4
+console.log(typeof undefined);
+
+// 5
+console.log(null == undefined);
+
+// 6
+console.log(null === undefined);
+
+// 7
+let strNull = "null";
+console.log(strNull);
+
+// 8
+let strUndefined = "undefined";
+console.log(strUndefined);
