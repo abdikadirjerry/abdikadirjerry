e4eeeec60643a4fab3c07315551b3553c5f43522
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-04 23:00:03 +0300

    feat: complete easy mixed drills

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-03-data-types/04-mini-drills.js
@@ -0,0 +1,22 @@
+// ============================================
+// 04-mini-drills.js
+// ============================================
+
+// ===============================
+// 🟢 Easy (1–6)
+// ===============================
+
+let p1 = 10;
+let p2 = "JS";
+let p3 = true;
+let p4 = null;
+let p5 = undefined;
+
+console.log(typeof p1, typeof p2, typeof p3, typeof p4, typeof p5);
+
+p1 = "ten";
+p3 = 0;
+
+let n = null;
+let u;
+
