5387425fc4013dda0e9f1bfda75577be52f15aa3
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-04 22:42:24 +0300

    feat: complete easy primitives challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-03-data-types/01-primitives-typeof.js
@@ -0,0 +1,44 @@
+// ==============================
+// Day 3 — Primitives & typeof
+// ==============================
+
+// EASY (1-10)
+
+//Challenge 1
+let age = 25;
+console.log(typeof age);
+
+// challenge 2
+let name = "Jerry";
+console.log(typeof name);
+
+// challenge 3
+let isStudent = true;
+console.log(typeof isStudent);
+
+// challenge 4
+let number = 7;
+console.log(typeof number);
+
+// challenge 5
+let empty = "";
+console.log(typeof empty);
+
+// challenge 6
+let zero = 0;
+console.log(typeof zero);
+
+// challenge 7
+let withOutAssign;
+console.log(typeof withOutAssign);
+
+// challenge 8
+let nul = null;
+console.log(typeof nul);
+
+// challenge 9
+let notANumber = NaN;
+console.log(typeof notANumber);
+
+// challenge 10
+let isOnline = "true";
