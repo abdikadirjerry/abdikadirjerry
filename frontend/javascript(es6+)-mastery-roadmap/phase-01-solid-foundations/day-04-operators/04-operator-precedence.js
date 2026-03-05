5cd995be15a07643118655803514e79445035aae
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-05 19:27:40 +0300

    feat: complete easy precedence operators challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-04-operators/04-operator-precedence.js
@@ -0,0 +1,25 @@
+/*
+=====================================
+04 — Operator Precedence
+Order of operations
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===")
+
+console.log(5 + 3 * 2)
+console.log((5 + 3) * 2)
+console.log(10 - 4 + 2)
+console.log(8 / 2 * 3)
+console.log(2 + 3 + 4 * 2)
+console.log(6 * 2 + 5)
+console.log(20 - 5 * 2)
+console.log((20 - 5) * 2)
+console.log(100 / 10 + 5)
+console.log(3 * 3 * 3)
+
