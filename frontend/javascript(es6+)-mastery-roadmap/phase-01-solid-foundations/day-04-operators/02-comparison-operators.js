fc9fcab4b545bb11134808c508118dac77045178
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-05 19:28:54 +0300

    feat: complete intermediate comparison challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-04-operators/02-comparison-operators.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-04-operators/02-comparison-operators.js
@@ -23,3 +23,38 @@ console.log(8 != 9)
 console.log("5" !== 5)
 console.log(100 > 50)
 
+
+// ===============================
+// 🟡 Intermediate (11–20)
+// ===============================
+
+console.log("=== INTERMEDIATE ===")
+
+let num = 120
+console.log(num > 100)
+
+let age = 19
+console.log(age >= 18)
+
+let a = 5
+let b = 10
+console.log(a > b)
+
+let value = 25
+console.log(value > 10 && value < 50)
+
+let temp = -5
+console.log(temp < 0)
+
+console.log("apple" < "banana")
+
+console.log(null == undefined)
+
+console.log(null === undefined)
+
+let score1 = 80
+let score2 = 90
+console.log(score1 > score2)
+
+console.log(0 === 0)
+
