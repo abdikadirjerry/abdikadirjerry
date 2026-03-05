03a81dbbdb7b981dc21da7bc9810204c5719e1e9
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-05 19:26:28 +0300

    feat: complete easy comparison operators challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-04-operators/02-comparison-operators.js
@@ -0,0 +1,25 @@
+/*
+=====================================
+02 — Comparison Operators
+>, <, >=, <=, ==, ===
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===")
+
+console.log(10 > 5)
+console.log(7 < 3)
+console.log(5 == 5)
+console.log(10 == "10")
+console.log(10 === "10")
+console.log(20 >= 20)
+console.log(15 <= 10)
+console.log(8 != 9)
+console.log("5" !== 5)
+console.log(100 > 50)
+
