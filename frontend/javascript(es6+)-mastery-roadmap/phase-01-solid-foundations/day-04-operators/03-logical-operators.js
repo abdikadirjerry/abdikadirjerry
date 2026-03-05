6f1fe1c3bc9faa9d9d6028dd766ad1477f736366
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-05 19:27:05 +0300

    feat: complete easy logical operators challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-04-operators/03-logical-operators.js
@@ -0,0 +1,28 @@
+/*
+=====================================
+03 — Logical Operators
+&& || !
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===")
+
+console.log(true && true)
+console.log(true && false)
+console.log(false || true)
+console.log(false || false)
+console.log(!true)
+console.log(!false)
+
+console.log(5 > 2 && 10 > 3)
+console.log(3 > 10 || 5 < 8)
+
+console.log((7 > 2) && (9 > 5))
+console.log((1 > 3) || (4 < 6))
+
+
