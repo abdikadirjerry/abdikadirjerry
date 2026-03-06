1835d76a1c98e95ab009a9521c0ebcbc3b564cd1
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-06 22:25:33 +0300

    feat(coercion): complete easy Coercion Puzzles challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-05-type-coercion/04-coercion-puzzles.js
@@ -0,0 +1,43 @@
+/*
+=====================================
+03 — Loose vs Strick equality Coercion (30)
+Operators: + - * / % **
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===");
+
+// 1. "5" + 2
+console.log("5" + 2);
+
+// 2. "5" - 2
+console.log("5" - 2);
+
+// 3. "5" * "2"
+console.log("5" * "2");
+
+// 4. "5" / "2"
+console.log("5" / "2");
+
+// 5. true + 1
+console.log(true + 1);
+
+// 6. false + 1
+console.log(false + 1);
+
+// 7. true + "1"
+console.log(true + "1");
+
+// 8. "10" - true
+console.log("10" - true);
+
+// 9. "10" + true
+console.log("10" + true);
+
+// 10. [] + []
+console.log([] + []);
