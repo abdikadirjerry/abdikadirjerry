74884294b398f8afde6508905d2538da402704c7
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-06 22:17:13 +0300

    feat(coercion): complete easy loose vs strick equality challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-05-type-coercion/03-loose-vs-strict-equality.js
@@ -0,0 +1,43 @@
+/*
+=====================================
+03 — Loose vs Strick equality Coercion (30)
+Operators: + - * / % **
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===");
+// 1. 5 == "5"
+console.log(5 == "5");
+
+// 2. 5 === "5"
+console.log(5 === "5");
+
+// 3. 0 == false
+console.log(0 == false);
+
+// 4. 0 === false
+console.log(0 === false);
+
+// 5. "" == 0
+console.log("" == 0);
+
+// 6. "" === 0
+console.log("" === 0);
+
+// 7. "10" == 10
+console.log("10" == 10);
+
+// 8. "10" === 10
+console.log("10" === 10);
+
+// 9. null == undefined
+console.log(null == undefined);
+
+// 10. null === undefined
+console.log(null === undefined);
+
