54d3413b588d1652c83d443ba50c0508af76bf23
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-06 22:08:13 +0300

    feat(coercion): complete easy boolean coercion challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-05-type-coercion/02-boolean-coercion.js
@@ -0,0 +1,43 @@
+/*
+=====================================
+01 — Boolean Coercion (30)
+Operators: + - * / % **
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===")
+
+// 1. Boolean(0)
+console.log(Boolean(0));
+
+// 2. Boolean(1)
+console.log(Boolean(1));
+
+// 3. Boolean("")
+console.log(Boolean(""));
+
+// 4. Boolean("hello")
+console.log(Boolean("hello"));
+
+// 5. Boolean(null)
+console.log(Boolean(null));
+
+// 6. Boolean(undefined)
+console.log(Boolean(undefined));
+
+// 7. Boolean(NaN)
+console.log(Boolean(NaN));
+
+// 8. Boolean([])
+console.log(Boolean([]));
+
+// 9. Boolean({})
+console.log(Boolean({}));
+
+// 10. Boolean("false")
+console.log(Boolean("false"));
