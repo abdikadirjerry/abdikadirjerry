dcd7f0c46090581db861142dfa2234bcaad4a5d2
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-07 13:27:57 +0300

    feat(control-flow): solve easy if-statement challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-06-control-flow/01-if-statements.js
@@ -0,0 +1,82 @@
+/*
+=====================================
+01 — if statement (30)
+=====================================
+*/
+
+
+// ===============================
+// 🟢 Easy (1–10)
+// ===============================
+
+console.log("=== EASY ===");
+
+//1. Check if number > 10
+let num = 12;
+
+if (num > 10) {
+  console.log("You're number is greater than 10 ");
+}
+
+//2. Check if age >= 18
+let age = 23;
+
+if (age >= 18) {
+  console.log("You're older");
+}
+
+//3. Check if temperature > 30
+let temp = 45;
+
+if (temp > 30) {
+  console.log("It's hot today");
+}
+
+//4. Check if score >= 50
+let score = 78;
+
+if (score >= 50) {
+  console.log("You passed in the exam");
+}
+
+//5. Check if number is positive
+let numb = 12;
+
+if (numb > 0) {
+    console.log("you're number is positive");
+}
+
+//6. Check if number is negative
+let numb2 = -12;
+
+if (numb2 < 0) {
+  console.log("You're number is negative");
+}
+
+//7. Check if password length > 8
+let pass = 123456789;
+
+if (pass > 8) {
+  console.log("You're password is strong");
+}
+
+//8. Check if user is logged in
+let isLogged = true;
+
+if (isLogged === true) {
+  console.log("You're logged in")
+}
+
+//9. Check if price > 100
+let price = 134;
+
+if (price > 100) {
+  console.log("You get a 5% of discount");
+}
+
+//10. Check if name is "Jerry"
+let name = "Jerry"
+
+if (name === "Jerry") {
+  console.log(`You're name is ${name}`);
+}
