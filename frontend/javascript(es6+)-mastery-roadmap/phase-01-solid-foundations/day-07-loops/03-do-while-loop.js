690211564110058a5a1e8e84f6487433055271d7
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-08 23:47:45 +0300

    feat(loops): solve intermediate do while loop challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/03-do-while-loop.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/03-do-while-loop.js
@@ -39,3 +39,54 @@ do {
   console.log(str[i]);
   i++;
 } while (i < str.length);
+
+
+// ===============================
+// 🟡 Intermediate (5–8)
+// ===============================
+
+console.log("=== INTERMEDIATE ===");
+
+//5. Count how many numbers from 1–50 are even
+// let i = 1;
+let count = 0;
+do {
+  if (i % 2 === 0) {
+    count++;
+  }
+  i++;
+} while (i <= 50);
+console.log(count);
+
+//6. Calculate sum of numbers from 1–30
+// let i = 1;
+let sum = 0;
+do {
+  sum += i;
+  i++;
+} while (i <= 30);
+console.log(sum);
+
+//7. Print numbers until the sum becomes greater than 100
+let sum1 = 0;
+// let i = 1;
+do {
+  sum += i;
+  console.log(i);
+  i++;
+} while (sum <= 100);
+
+//8. Find largest number in an array
+let numbers = [12, 45, 7, 23, 89, 34];
+let largest = numbers[0];
+// let i = 1;
+
+do {
+  if (numbers[i] > largest) {
+    largest = numbers[i];
+  }
+  i++;
+} while (i < numbers.length);
+
+console.log(largest);
+
