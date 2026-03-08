a3ae1f1ab724ee539d373d61d75891ff0ff4ca32
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-08 23:37:53 +0300

    feat(loops): solve intermediate while loop challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/02-while-loop.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/02-while-loop.js
@@ -12,7 +12,7 @@
 //1. Use a while loop to print numbers 1 to 10.
 console.log("=== EASY ===");
 
-let i = 1;
+// let i = 1;
 
 while (i <= 10) {
     console.log(i);
@@ -20,7 +20,7 @@ while (i <= 10) {
 }
 
 //2. Create a while loop that prints numbers 10 down to 1.
-let i = 10;
+// let i = 10;
 
 while (i >= 1) {
     console.log(i);
@@ -28,7 +28,7 @@ while (i >= 1) {
 }
 
 //3. Create a while loop that prints numbers 10 down to 1.
-let i = 1;
+// let i = 1;
 
 while (i <= 15) {
     if (i % 2 === 1) {
@@ -39,7 +39,7 @@ while (i <= 15) {
 
 //4. Use a while loop to calculate the sum of numbers from 1 to 20.
 
-let i = 1;
+// let i = 1;
 let sum = 0;
 
 while (i <= 20) {
@@ -56,7 +56,7 @@ console.log(sum)
 console.log("=== INTERMEDIATE ===");
 
 //5. Create an array of numbers and use a while loop to print every element in the array.
-let i = 0;
+// let i = 0;
 let arr = [1, 2, 3, 4, 5];
 
 while (i <= arr.length) {
@@ -65,10 +65,36 @@ while (i <= arr.length) {
 }
 
 //6. Write a program that keeps doubling a number starting from 1 until it becomes greater than 200.
-let i = 1;
+// let i = 1;
 
 while (i <= 200) {
     i = i * 2;
     console.log(i);
     i++
 }
+
+//7. Count how many numbers in this array are greater than 10:
+let numbers = [5, 12, 8, 21, 3, 17];
+let count = 0;
+// let i = 0;
+
+while (i < numbers.length) {
+  if (numbers[i] > 10) {
+    count++;
+  }
+  i++;
+}
+
+console.log(count);
+
+//8. Use a while loop to reverse a word.
+let word = "coding";
+let reversed = "";
+let i = word.length - 1;
+
+while (i >= 0) {
+  reversed += word[i];
+  i--;
+}
+
+console.log(reversed);
