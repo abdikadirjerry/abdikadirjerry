1f5ced43e3272a59db901ef04e395f65f924df6d
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-08 23:31:09 +0300

    feat(loops): solve intermediate for loop challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/01-for-loop.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-07-loops/01-for-loop.js
@@ -39,3 +39,51 @@ for (let i = 1; i <= 50; i++) {
 
 }
 console.log(sum);
+
+
+// ===============================
+// 🟡 Intermediate (5–8)
+// ===============================
+
+console.log("=== INTERMEDIATE ===");
+
+//5. Loop through an array
+let arr = ["mango", "avocado", "banana"];
+
+for (let i = 0; i <= arr.length; i++) {
+    console.log(arr[i]);
+}
+
+//6. Find the largest number
+let numbers = [12, 45, 7, 23, 89, 34];
+
+let largest = numbers[0];
+
+for (let i = 1; i <= numbers.length; i++) {
+    if (numbers[i] > largest) {
+        largest = numbers[i]
+    }
+} 
+
+console.log(largest);
+
+//7. Reverse a string manually
+
+let word = "developer";
+let reversed = "";
+
+for (let i = word.length -1; i >= 0; i--) {
+    reversed += word[i];
+}
+console.log(reversed)
+
+let js = "javascript";
+let count = 0;
+
+for (let i = 0; i <= js.length; i++) {
+    if (js[i] === "a" || js[i] === "e" || js[i] === "i" || js[i] === "0" || js[i] === "u") {
+        count++
+    }
+}
+console.log(count);
+
