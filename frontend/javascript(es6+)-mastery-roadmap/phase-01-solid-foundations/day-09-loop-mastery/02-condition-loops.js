672a9a336f56bb8ef90e1a4003c136227d4c52a6
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-10 21:35:47 +0300

    feat(loops): complete loop with condition challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-09-loop-mastery/02-condition-loops.js
@@ -0,0 +1,28 @@
+
+// 5. Print even numbers
+for (let i = 1; i <= 50; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+// 6. Print odd numbers
+for (let i = 1; i <= 50; i++) {
+    if (!i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+// 7. Print numbers divisible by 5
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log(i)
+    }
+}
+
+// 8. Print numbers divisible by 3 and 5
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(i)
+    }
+}
