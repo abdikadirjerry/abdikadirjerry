26d1d5dd752c2e042eab0023f82349cc471832e0
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-10 23:54:29 +0300

    feat(loops): complete solvig loop challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-09-loop-mastery/problem2-average.js
@@ -0,0 +1,10 @@
+// Problem 2 — Calculate Average
+const num = [10, 20, 30, 40, 50];
+let sum = 0;
+let average = 0;
+
+
+for (let i = 0; i < num.length; i++) {
+    sum += num[i];
+}
+console.log(average = sum / num.length);
