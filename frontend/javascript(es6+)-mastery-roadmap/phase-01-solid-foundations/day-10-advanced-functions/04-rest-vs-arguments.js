fbb2b04249ed61aa22baa524e495885d5347db6b
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-11 22:59:28 +0300

    feat(functions): solve intermediate function rest vs parameters challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-10-advanced-functions/04-rest-vs-arguments.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-10-advanced-functions/04-rest-vs-arguments.js
@@ -25,3 +25,44 @@ function compareSum() {
   console.log("Arguments sum:", argsSum);
   console.log("Rest sum:", restSum);
 }
+
+console.log("=== INTERMEDIATE ===");
+
+// 4
+function maxArguments() {
+  let max = arguments[0];
+  for (let i = 1; i < arguments.length; i++) {
+    if (arguments[i] > max) max = arguments[i];
+  }
+  return max;
+}
+
+function maxRest(...numbers) {
+  let max = numbers[0];
+  for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] > max) max = numbers[i];
+  }
+  return max;
+}
+
+// 5
+function sumArgumentsArray() {
+  let arr = [];
+  for (let i = 0; i < arguments.length; i++) {
+    arr.push(arguments[i]);
+  }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+// 6
+function filterGreaterThan10(...numbers) {
+  let result = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 10) result.push(numbers[i]);
+  }
+  return result;
+}
