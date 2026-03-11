7aab619c216aa023230692fda91d77f31aefcf91
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-11 23:03:03 +0300

    feat(functions): solve intermedaite function real word challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-10-advanced-functions/05-real-world-examples.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-10-advanced-functions/05-real-world-examples.js
@@ -15,3 +15,28 @@ function celsiusToFahrenheit(celsius) {
 function formatUsername(username) {
   return username.toUpperCase();
 }
+
+console.log("=== INTERMEDIATE ===");
+
+// 4
+function cartTotal() {
+  let total = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  }
+  return total;
+}
+
+// 5
+function passwordStrength(password) {
+  if (password.length < 6) return "weak";
+  if (password.length < 10) return "medium";
+  return "strong";
+}
+
+// 6
+function formatEmail(email) {
+  if (email.indexOf("@") !== -1) return email;
+  return "Invalid email";
+}
+
