b3beb45b8479eb7a5aeb657e7a58e0de5243a84d
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-11 22:52:22 +0300

    feat(functions): solve easy and intermediate function default arguments challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-10-advanced-functions/02-arguments-object.js
@@ -0,0 +1,59 @@
+console.log("=== EASY ===");
+// 1
+function showArguments() {
+  for (let i = 0; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+showArguments(10, 20, 30);
+
+// 2
+function countArgs() {
+  return arguments.length;
+}
+console.log(countArgs(1, 2, 3, 4));
+
+// 3
+function firstLast() {
+  console.log("First:", arguments[0]);
+  console.log("Last:", arguments[arguments.length - 1]);
+}
+firstLast(10, 20, 30, 40);
+
+console.log("=== INTERMEDIATE ===");
+// 4
+function sumNumbers() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    sum += arguments[i];
+  }
+  return sum;
+}
+console.log(sumNumbers(1, 2, 3, 4));
+
+// 5
+
+function findMax() {
+  let largest = arguments[0];
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > largest) {
+      largest = arguments[i];
+    }
+  }
+  return largest;
+}
+
+console.log(findMax(1, 5, 11, 43, 5));
+
+// 6
+function countEven() {
+  let sum = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] % 2 === 0) {
+      sum++;
+    }
+  }
+  return sum;
+}
+
+console.log(countEven(2, 5, 6, 9, 10));
