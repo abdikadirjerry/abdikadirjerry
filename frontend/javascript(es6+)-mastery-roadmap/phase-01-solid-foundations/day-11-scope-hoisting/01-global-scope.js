5b7fc6d83cd827380bb8e863a413ebb5f5dc4d67
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-12 22:33:45 +0300

    add global scope example

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-11-scope-hoisting/01-global-scope.js
@@ -0,0 +1,11 @@
+// Global Scope Example
+
+let websiteName = "My Learning Journey";
+
+function showWebsite() {
+  console.log("Inside function:", websiteName);
+}
+
+console.log("Outside function:", websiteName);
+
+showWebsite();
