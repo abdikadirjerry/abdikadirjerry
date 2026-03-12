a58c9990038d37604d731292dce9455240b34f3c
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-12 22:34:00 +0300

    add function scope example

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-11-scope-hoisting/02-function-scope.js
@@ -0,0 +1,12 @@
+// Function Scope Example
+
+function greetUser() {
+  let message = "Hello JavaScript";
+
+  console.log(message); // accessible here
+}
+
+greetUser();
+
+// console.log(message);
+// This would cause an error because message is inside the function
