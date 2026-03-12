25892188ccd1d4a525f73018959e7e48224ce315
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-12 22:34:52 +0300

    add temporal dead zone example

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-11-scope-hoisting/05-tdz.js
@@ -0,0 +1,16 @@
+// Temporal Dead Zone Example
+
+// console.log(score);
+// This would cause an error because of TDZ
+
+let score = 100;
+
+console.log(score);
+
+// Another TDZ example
+
+{
+  // console.log(price); // TDZ error
+  let price = 50;
+  console.log(price);
+}
