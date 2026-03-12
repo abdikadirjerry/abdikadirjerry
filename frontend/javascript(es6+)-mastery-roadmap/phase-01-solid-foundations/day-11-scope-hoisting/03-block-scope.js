db3aecb39ca5febb3641188f40c9dd656a872211
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-12 22:34:16 +0300

    add block scope example

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-11-scope-hoisting/03-block-scope.js
@@ -0,0 +1,12 @@
+// Block Scope Example
+
+if (true) {
+  let age = 25;
+  const city = "Hargeisa";
+
+  console.log("Inside block:", age, city);
+}
+
+// console.log(age);
+// console.log(city);
+// These would cause errors because they are block scoped
