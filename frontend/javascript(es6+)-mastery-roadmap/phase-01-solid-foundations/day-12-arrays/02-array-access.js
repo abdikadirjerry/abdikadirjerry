948eb83a76a120a9f9a3f8745c5ec34f118ac384
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-13 22:13:48 +0300

    feat (arrays): solve array access challneges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-12-arrays/02-array-access.js
@@ -0,0 +1,12 @@
+// Challenge 1 — First Element
+const fruits = ["apple", "banana", "orange", "mango"];
+console.log(fruits[0]);
+
+// Challenge 2 — Last Element
+const names = ["john", "zara", "abdikadir", "hala"];
+console.log(names[names.length - 1]);
+
+// Challenge 3 — Modify an Element
+const cars = ["BMW", "Mercedes", "Toyota", "Honda"];
+cars[3] = "Nissan";
+console.log(cars);
