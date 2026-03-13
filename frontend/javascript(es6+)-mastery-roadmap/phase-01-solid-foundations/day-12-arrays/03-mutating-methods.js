631cc95190de7e73038da35c27dc672f5ec14547
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-13 22:14:32 +0300

    feat (arrays): solve array mutating challneges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-12-arrays/03-mutating-methods.js
@@ -0,0 +1,17 @@
+// Challenge 1 — push()
+const numbers = [1, 2, 3];
+numbers.push(4);
+console.log(numbers);
+
+// Challenge 2 — pop()
+const num = [1, 2, 3, 4];
+num.pop();
+console.log(num);
+
+// Challenge 3 — shift() and unshift()
+const animals = ["cat", "dog", "lion"];
+animals.unshift("tiger");
+console.log(animals);
+
+animals.shift();
+console.log(animals);
