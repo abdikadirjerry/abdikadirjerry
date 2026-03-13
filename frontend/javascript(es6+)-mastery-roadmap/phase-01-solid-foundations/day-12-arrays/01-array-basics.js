3f834ef82c58df41c80bf0ef737cc16b59b56daf
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-13 22:12:56 +0300

    feat (arrays): solve array basics challneges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-12-arrays/01-array-basics.js
@@ -0,0 +1,14 @@
+// Challenge 1 — Create an Array
+const arr = ["red", "blue", "green", "yellow"];
+
+console.log(arr);
+
+// Challenge 2 — Array Length
+const num = [33, 12, 43, 3, 7, 80];
+
+console.log(num.length);
+
+// Challenge 3 — Mixed Data Types
+const profile = ["Abdikadir", 23, true, "Germany"];
+
+console.log(profile);
