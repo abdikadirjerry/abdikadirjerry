2849a8e2b18518f76f9aa4c26ce0ff34104f99ce
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-13 22:15:20 +0300

    feat (arrays): solve array non mutating method challneges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-12-arrays/04-non-mutating-methods.js
@@ -0,0 +1,16 @@
+// Challenge 1 — slice()
+const numbers = [10, 20, 30, 40, 50];
+const part = numbers.slice(1, 4);
+console.log(part);
+
+// Challenge 2 — concat()
+const a = [1, 2];
+const b = [3, 4];
+
+const c = a.concat(b);
+console.log(c);
+
+// Challenge 3 — Copy Array
+const original = [5, 10, 15];
+const copy = original.slice(0, 3);
+console.log(copy);
