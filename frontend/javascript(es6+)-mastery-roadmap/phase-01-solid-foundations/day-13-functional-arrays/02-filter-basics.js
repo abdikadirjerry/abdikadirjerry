7a33cedb5aefc4ff769fb6f7bb0f0c9138eb756d
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-14 21:19:33 +0300

    feat(functional-arrays): solve filter basics challenges

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-13-functional-arrays/02-filter-basics.js
@@ -0,0 +1,17 @@
+// challenge 1
+const number = [1, 2, 3, 4, 5, 6];
+const even = number.filter((num) => num % 2 === 0);
+
+console.log(even);
+
+// challenge 2
+const numbers = [5, 12, 8, 20, 3];
+const greater = numbers.filter((num) => num > 10);
+
+console.log(greater);
+
+// challenge 3
+const animals = ["cat", "elephant", "dog", "giraffe"];
+const longer = animals.filter((num) => num.length > 4);
+
+console.log(longer);
