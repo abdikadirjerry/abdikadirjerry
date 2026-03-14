549cafc685905a29583fef2c7cf4057d18988f76
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-14 21:23:07 +0300

    feat(functional-arrays): solve clean code map and filter basics challenges

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-13-functional-arrays/04-map-filter-combo.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-13-functional-arrays/04-map-filter-combo.js
@@ -1,48 +1,38 @@
 // Challenge 1
-// Multiply only even numbers by 2
-
 const numbers1 = [1, 2, 3, 4, 5];
 
 const result1 = numbers1
-  .filter(function(num) {
+  .filter(function (num) {
     return num % 2 === 0;
   })
-  .map(function(num) {
+  .map(function (num) {
     return num * 2;
   });
 
-console.log(result1); // [4, 8]
-
-
+console.log(result1);
 
 // Challenge 2
-// Get uppercase names longer than 3 letters
-
 const names = ["ali", "sara", "mo", "john"];
 
 const result2 = names
-  .filter(function(name) {
+  .filter(function (name) {
     return name.length > 3;
   })
-  .map(function(name) {
+  .map(function (name) {
     return name.toUpperCase();
   });
 
-console.log(result2); // ["SARA", "JOHN"]
-
-
+console.log(result2);
 
 // Challenge 3
-// Square numbers greater than 5
-
 const numbers2 = [2, 6, 3, 8];
 
 const result3 = numbers2
-  .filter(function(num) {
+  .filter(function (num) {
     return num > 5;
   })
-  .map(function(num) {
+  .map(function (num) {
     return num * num;
   });
 
-console.log(result3); // [36, 64]
+console.log(result3);
