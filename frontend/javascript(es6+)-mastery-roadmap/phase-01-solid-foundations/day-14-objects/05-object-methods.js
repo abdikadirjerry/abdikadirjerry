5cd74d6f24ddbcae7c06aade47e31a4d500642fa
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-15 21:51:58 +0300

    feat(objects): solve object methods challenge

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-14-objects/05-object-methods.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-14-objects/05-object-methods.js
@@ -9,6 +9,7 @@ const student = {
 
 student.greet();
 
+
 // challenge 2
 const book = {
   title: "JavaScript Basics",
@@ -22,24 +23,21 @@ const book = {
 
 console.log(book.info());
 
+
 // challenge 3
 const calc = {
-  // Add two numbers
   add: function (a, b) {
     return a + b;
   },
 
-  // Subtract two numbers
   subtract: function (a, b) {
     return a - b;
   },
 
-  // Multiply two numbers
   multiply: function (a, b) {
     return a * b;
   },
 
-  // Divide two numbers
   divide: function (a, b) {
     if (b === 0) {
       return "Cannot divide by zero";
@@ -48,9 +46,9 @@ const calc = {
   },
 };
 
-// Testing the calculator
-console.log(calc.add(2, 3)); // 5
-console.log(calc.subtract(10, 4)); // 6
-console.log(calc.multiply(3, 5)); // 15
-console.log(calc.divide(10, 2)); // 5
-console.log(calc.divide(5, 0)); // Cannot divide by zero
+
+console.log(calc.add(2, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 5));
+console.log(calc.divide(10, 2));
+console.log(calc.divide(5, 0));
