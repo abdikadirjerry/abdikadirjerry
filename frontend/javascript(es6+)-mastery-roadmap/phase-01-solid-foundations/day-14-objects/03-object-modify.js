9040e1a0563f3da1877c166a4506861d4a1a9b9a
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-15 21:43:10 +0300

    create object modify file

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-14-objects/03-object-modify.js
@@ -0,0 +1,33 @@
+// challenge 1
+const person = {
+  name: "ahmed",
+  age: 25,
+  phone: 12349876,
+};
+
+person.phone = 123456789;
+
+console.log(person);
+
+// challenge 2
+const student = {
+  name: "mohamed",
+  age: 22,
+  isGraduate: true,
+};
+
+student.phone = 123456789;
+
+console.log(student);
+
+// challenge 3
+const book = {
+  title: "think fast and slow",
+  author: "john de",
+  pages: 254,
+  isAvailable: true,
+};
+
+delete book.isAvailable;
+
+console.log(book);
