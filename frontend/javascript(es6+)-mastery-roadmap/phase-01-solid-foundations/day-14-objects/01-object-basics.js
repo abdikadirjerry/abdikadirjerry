3313caf91942b2430e5dd9d565d78f3396301c56
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-15 21:42:22 +0300

    create object basics file

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-14-objects/01-object-basics.js
@@ -0,0 +1,26 @@
+// challenge 1
+const car = {
+  brand: "BMW",
+  model: "M5 CS F90",
+  year: "2021",
+};
+
+console.log(car);
+
+// challenge 2
+const student = {
+  name: "ashton",
+  age: "23",
+  isGraduated: true,
+};
+
+console.log(student);
+
+// challenge 3
+const book = {
+  title: "JavaScript Basics",
+  author: "John Doe",
+  pages: 250,
+};
+
+console.log(Object.keys(book));
