b2b77efee5a46baa58338d6f75784318d173c478
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-15 21:42:41 +0300

    create object access file

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-14-objects/02-object-access.js
@@ -0,0 +1,19 @@
+// challenge 1
+const user = {
+  username: "najio",
+  email: "najio@example.com",
+};
+
+console.log(user.username);
+
+// challenge 2
+const status = {
+  name: "mohamed",
+  address: "main road",
+};
+
+console.log(status["name"]);
+
+// challenge 3
+const key = "address";
+console.log(status[key]);
