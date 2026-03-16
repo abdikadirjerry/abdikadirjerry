beb0e1d0cfd23e3c3e948ebe3905d881066ddd00
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-16 22:13:02 +0300

    feat(distructuring-spread): solve default spread array challenge

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-15-destructuring-spread-rest/04-spread-arrays.js
@@ -0,0 +1,21 @@
+// Challenge 1 — Clone array
+const arr = [1, 2, 3];
+
+const clone = [...arr];
+
+console.log(clone);
+
+// Challenge 2 — Merge arrays
+const a = [1, 2];
+const b = [3, 4];
+
+const merged = [...a, ...b];
+
+console.log(merged);
+
+// Challenge 3 — Add element using spread
+const numbers = [10, 20, 30];
+
+const newNumbers = [...numbers, 40];
+
+console.log(newNumbers);
