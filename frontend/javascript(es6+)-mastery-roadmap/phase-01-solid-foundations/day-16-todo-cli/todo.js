17949784d3a220505e624c3c842ad597f5c9516d
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-17 20:26:06 +0300

    feat(todo): solve add task in todo app

--- frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-16-todo-cli/todo.js
+++ frontend/javascript(es6+)-mastery-roadmap/phase-01-solid-foundations/day-16-todo-cli/todo.js
@@ -1,10 +1,7 @@
 const command = process.argv[2];
 const value = process.argv[3];
 
-console.log(command, value);
-
-// Task storage
-let tasks = [];
+const tasks = [];
 
 if (command === "add") {
   const task = {
@@ -13,38 +10,4 @@ if (command === "add") {
   };
   tasks.push(task);
   console.log(`Task added: ${value}`);
-}
-
-// List Tasks
-if (command === "list") {
-  console.log("Your Tasks:");
-
-  for (let i = 0; i < tasks.length; i++) {
-    const status = tasks[i].completed ? "✅" : "❌";
-    console.log(`${i + 1}. ${tasks[i].text} ${status}`);
-  }
-}
-
-// Mark as Done
-if (command === "done") {
-  const index = value - 1;
-
-  if (tasks[index]) {
-    tasks[index].completed = true;
-    console.log(`Task ${value} marked as done`);
-  } else {
-    console.log("Task not found");
-  }
-}
-
-// Delete task
-if (command === "delete") {
-  const index = value - 1;
-
-  if (tasks[index]) {
-    tasks.splice(index, 1);
-    console.log(`Task ${value} deleted`);
-  } else {
-    console.log("Task not found");
-  }
-}
+} 
