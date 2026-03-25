cdcf5f0dbb0b3ea23452d1169c5a943fc677211b
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-25 23:25:13 +0300

    docs: explain O(n) linear time using loop example

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-02-deep-javascript/day-24-bigO-notation/02-on-linear.js
@@ -0,0 +1,17 @@
+// 🟢 Challenge — O(n)
+
+const arr = [1, 2, 3, 4, 5];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+/*
+📌 Time Complexity: O(n)
+
+📌 Explanation:
+- Loop runs once for each element.
+- If array has n elements → loop runs n times.
+
+👉 More data = more steps (linear growth)
+*/
