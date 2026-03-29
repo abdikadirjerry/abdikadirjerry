a9e07d782ec01af443a98338afc29c0457c0fedd
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-03-29 20:31:44 +0300

    feat: simulate API returning JSON data

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-02-real-world-js-web-foundations/day-24-api-design/01-api-simulation.js
@@ -0,0 +1,10 @@
+// Challenge — Fake API Response
+
+function getUsers() {
+  return [
+    { id: 1, name: "Ali" },
+    { id: 2, name: "Sara" },
+  ];
+}
+
+console.log("API Response:", getUsers());
