5153d6be078319c9fe8f31f04af1b4c6ad17b623
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-04-13 19:31:38 +0300

    feat: implement retry mechanism and debounce utility

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-03-async-mastery/async-task-manager/utils.js
@@ -0,0 +1,18 @@
+export async function retry(fn, times = 3) {
+  for (let i = 0; i < times; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === times - 1) throw err;
+    }
+  }
+}
+
+// debounce
+export function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn.apply(this, args), delay);
+  };
+}
