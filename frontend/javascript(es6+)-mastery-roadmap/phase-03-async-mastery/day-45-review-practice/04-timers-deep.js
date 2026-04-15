fa0b2d6040c77be6ba7e6a617672c85a3505cff8
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-04-15 23:06:02 +0300

    docs: explain FIFO behavior of callback queue

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-03-async-mastery/day-45-review-practice/04-timers-deep.js
@@ -0,0 +1,13 @@
+// Problem 6
+
+setTimeout(() => {
+  console.log("Timeout 1");
+}, 0);
+
+setTimeout(() => {
+  console.log("Timeout 2");
+}, 0);
+
+console.log("End");
+
+// ❓ Is order guaranteed? Why?
