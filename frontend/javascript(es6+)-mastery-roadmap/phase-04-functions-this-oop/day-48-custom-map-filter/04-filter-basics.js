a4888cc5ede901f1b4b6d6b674e3a5051d0068b8
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-04-18 21:11:42 +0300

    docs: explain conditional filtering behavior

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-04-functions-this-oop/day-48-custom-map-filter/04-filter-basics.js
@@ -0,0 +1,12 @@
+// 04-filter-basics.js
+
+const numbers = [1, 2, 3, 4, 5];
+
+const evens = numbers.filter((num) => num % 2 === 0);
+
+console.log(evens);
+
+/*
+🧠 filter:
+- keeps items that return TRUE
+*/
