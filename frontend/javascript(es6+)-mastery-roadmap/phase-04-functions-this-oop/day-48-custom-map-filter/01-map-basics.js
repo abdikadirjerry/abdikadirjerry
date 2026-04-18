d7839549ea63bf0785b9534988c5d4d0d2f2501d
Abdikadir Jerry <abdikadirjerry@gmail.com>
2026-04-18 21:09:21 +0300

    docs: explain transformation behavior of map method

---
+++ frontend/javascript(es6+)-mastery-roadmap/phase-04-functions-this-oop/day-48-custom-map-filter/01-map-basics.js
@@ -0,0 +1,14 @@
+// 01-map-basics.js
+
+const numbers = [1, 2, 3, 4];
+
+const doubled = numbers.map((num) => num * 2);
+
+console.log(doubled);
+
+/*
+🧠 map:
+- loops through array
+- transforms each item
+- returns NEW array
+*/
